16057e82940b88f97516ba813c8971428c441912
mariamossama <85443737+mariamossama@users.noreply.github.com>
2024-07-06 14:35:24 +0300

    application structure

---
+++ PlaceYourStation/src/main/java/com/officeseatbooking/PlaceYourStation/PlaceYourStationApplication.java
@@ -0,0 +1,13 @@
+package com.officeseatbooking.PlaceYourStation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaceYourStationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaceYourStationApplication.class, args);
+	}
+
+}
